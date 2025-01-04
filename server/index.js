b3feac77ec67a3de496df0aa2b94fbeacdfc6563
phantomsoldierking <patilyogeshgouda@gmail.com>
2025-01-04 18:43:49 +0530

    qwref

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const ptyProcess = pty.spawn('bash', [], {
     name: 'xterm-color',
     cols: 80,
     rows: 30,
-    cwd: process.env.INIT_CWD + '/user',
+    cwd: '/user',
     env: process.env
 });
 
